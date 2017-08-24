25595ddc553cebe0c2338751a1efbb400d287e26
hew-and-eye <matthew.r.bernardo@gmail.com>
2017-08-24 09:09:43 -0400

    fixed scrolling bug and changed cursor when hovering over tabs

--- js/portfoliojs.js
+++ js/portfoliojs.js
@@ -8,8 +8,9 @@ let adjustFixedScrollbar = function () {
 let scrollDown = function () {
     scrollInterval = function () {
         window.scrollTo(0, document.body.scrollTop + 10);
-        if (document.body.scrollTop + clientHeight >= document.body.offsetHeight-10) {
-            console.log("yo");
+        if (document.body.scrollTop + clientHeight >= document.body.offsetHeight - 10) {
+            window.scrollTo(0, clientHeight);
+            //console.log("yo");
             clearInterval(id);
         }
         //console.log("hi");
@@ -17,4 +18,11 @@ let scrollDown = function () {
     let id = setInterval(scrollInterval, 1);
 }
 window.addEventListener("scroll", adjustFixedScrollbar);
-document.getElementById("about-tab").addEventListener("focus", scrollDown);
+document.getElementById("about-tab").addEventListener("click", scrollDown);
+
+window.onload = function () {
+    document.getElementById("default-focus").focus();
+    document.getElementById("about-tab").blur();
+    window.scrollTo(0, 0);
+    console.log("yo");
+}
