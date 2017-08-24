f2e3bd7001f28e1a5519032ca79cee0cfdb62d98
Matthew Bernardo <M1038980@mindtree.com>
2017-08-24 15:42:15 -0400

    added a bunch of assets and added a horizontally scrolling content region, with js to scroll it

--- js/portfoliojs.js
+++ js/portfoliojs.js
@@ -1,24 +1,27 @@
 ﻿let scrolledHeaderHeight = 0;
 let clientHeight = window.innerHeight;
-let adjustFixedScrollbar = function () {
+let adjustFixedScrollbar = function (evt, marginIndex) {
     scrolledHeaderHeight = Math.min(clientHeight * 0.12, document.body.scrollTop);
     document.getElementById("scrolled-header").style.height = scrolledHeaderHeight + "px";
+    if(document.body.scrollTop < clientHeight*0.4)
+        document.getElementById("content-scroller").style.marginLeft = 0;
 };
 
-let scrollDown = function () {
+let scrollDown = function (evt, marginIndex) {
     scrollInterval = function () {
         window.scrollTo(0, document.body.scrollTop + 10);
         if (document.body.scrollTop + clientHeight >= document.body.offsetHeight - 10) {
             window.scrollTo(0, clientHeight);
             //console.log("yo");
             clearInterval(id);
-        }
+        } 
+        document.getElementById("content-scroller").style.marginLeft = (-marginIndex*100) + "vw";
         //console.log("hi");
     }
     let id = setInterval(scrollInterval, 1);
 }
-window.addEventListener("scroll", adjustFixedScrollbar);
-document.getElementById("about-tab").addEventListener("click", scrollDown);
+window.addEventListener("scroll", (event) => {adjustFixedScrollbar(event, 1)});
+document.getElementById("about-tab").addEventListener("click", (event) => {scrollDown(event, 1)});
 
 window.onload = function () {
     document.getElementById("default-focus").focus();
