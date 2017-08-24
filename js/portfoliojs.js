7b28cc29d7a40a49dc83c1828aed20d760643f55
hew-and-eye <matthew.r.bernardo@gmail.com>
2017-08-24 08:51:25 -0400

    Basic layout elements and fancy scroll handling

---
+++ js/portfoliojs.js
@@ -0,0 +1,20 @@
+﻿let scrolledHeaderHeight = 0;
+let clientHeight = window.innerHeight;
+let adjustFixedScrollbar = function () {
+    scrolledHeaderHeight = Math.min(clientHeight * 0.12, document.body.scrollTop);
+    document.getElementById("scrolled-header").style.height = scrolledHeaderHeight + "px";
+};
+
+let scrollDown = function () {
+    scrollInterval = function () {
+        window.scrollTo(0, document.body.scrollTop + 10);
+        if (document.body.scrollTop + clientHeight >= document.body.offsetHeight-10) {
+            console.log("yo");
+            clearInterval(id);
+        }
+        //console.log("hi");
+    }
+    let id = setInterval(scrollInterval, 1);
+}
+window.addEventListener("scroll", adjustFixedScrollbar);
+document.getElementById("about-tab").addEventListener("focus", scrollDown);
