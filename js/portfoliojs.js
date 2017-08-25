f7e9ad4ebeb5b00107b6ae36fbbd85370e0447f7
Matthew Bernardo <M1038980@mindtree.com>
2017-08-25 14:23:32 -0400

    added vertical scrolling sections and content to the personal projects section

--- js/portfoliojs.js
+++ js/portfoliojs.js
@@ -17,6 +17,7 @@ let scrollDown = function (evt, marginIndex) {
             //document.getElementById("content-scroller").style.marginLeft = (-marginIndex * 100) + "vw";
         }
         document.getElementById("content-scroller").style.marginLeft = (-marginIndex * 100) + "vw";
+        //window.scrollTo(0, window.scrollTop);
         //console.log("hi");
     }
     let id = setInterval(scrollInterval, 1);
@@ -25,10 +26,10 @@ window.addEventListener("scroll", (event) => { adjustFixedScrollbar(event, 1) })
 document.getElementById("home-tab").addEventListener("click", (event) => { document.getElementById("content-scroller").style.marginLeft = 0; });
 document.getElementById("about-tab").addEventListener("click", (event) => {scrollDown(event, 1)});
 document.getElementById("skills-tab").addEventListener("click", (event) => { scrollDown(event, 2) });
+document.getElementById("projects-tab").addEventListener("click", (event) => { scrollDown(event, 3); });
 
 window.onload = function () {
     document.getElementById("default-focus").focus();
     document.getElementById("about-tab").blur();
     window.scrollTo(0, 0);
-    console.log("yo");
 }
